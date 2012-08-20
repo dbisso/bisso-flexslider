346ebd0861606b2b483ec05493b28562d5d52b68
Dan Bissonnet <dan@danisadesigner.com>
2012-08-20 12:06:07 +0100

    (split)Add fixed height to flexslider - fixes the height of the slider to the max height of the slides and adds vertical padding where needed.

--- bisso-flexslider.php
+++ bisso-flexslider.php
@@ -201,8 +201,33 @@ class Bisso_Flexslider {
 	function action_wp_footer ( ) {
 		echo "<script type='text/javascript'>
 jQuery('document').ready( function($){
-	$('.flexslider').flexslider(bissoFlexsliderSettings.flexsliderSettings);
-})
+
+	var fixHeight = function ( slider ) {
+		var maxHeight = 0,
+			heights = [];
+
+		// Find the max height needed
+		for ( var i = 0; i < slider.slides.length; i++ ) {
+			heights[i] = $(slider.slides[i]).height()
+			maxHeight = Math.max(maxHeight, heights[i]);
+		}
+
+		// Add vertical padding
+		for ( var i = 0; i < slider.slides.length; i++ ) {
+			$(slider.slides[i]).css('padding', (maxHeight - heights[i]) / 2 + 'px 0');
+			maxHeight = Math.max(maxHeight, heights[i]);
+		}
+
+		// Set the height on the viewport
+		slider.find('.slides').height( maxHeight );
+	}
+
+	bissoFlexsliderSettings.flexsliderSettings.start = fixHeight
+
+	var slider = $('.flexslider').flexslider(bissoFlexsliderSettings.flexsliderSettings).data('flexslider');
+	$(window).bind('resize', function() { fixHeight(slider) } );
+
+});
 		</script>";
 	}
 
