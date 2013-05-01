27163b90f774ca794dc6ebe3025aab7c5c29de62
Dan Bissonnet <dan@danisadesigner.com>
2013-05-01 18:54:07 +0100

    Use custom event to trigger loading of Flexslider

--- js/flexslider-init.js
+++ js/flexslider-init.js
@@ -1,6 +1,20 @@
 /**
  * Initialise the flexsliders on DOM ready
  */
+
+;(function($) {
+	$(document).on('bissoFlexSlider:init', function(event) {
+		var sliders = $('.flexslider').not('.js-init'),
+			slides = sliders.find('.slides > *');
+
+		slides.each( function(i, element) {
+			$(element).html( $(element).html().replace(/<!--/, '').replace(/-->/, '') );
+		} );
+
+		sliders.flexslider(bissoFlexsliderSettings.flexsliderSettings).addClass('js-init');
+	});
+}(jQuery));
+
 jQuery('document').ready( function($){
-	$('.flexslider').flexslider(bissoFlexsliderSettings.flexsliderSettings);
+	$(document).trigger('bissoFlexSlider:init');
 });
